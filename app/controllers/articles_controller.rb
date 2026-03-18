9df3afdf14d58595ed1d73873d4019bd3050646e
Basant Yadav <basantkgp22@gmail.com>
2026-03-19 02:24:44 +0530

    added functionality like creating new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,5 +5,28 @@ class ArticlesController < ApplicationController
    end
    def index
       @articles=Article.all;
-   end   
+   end
+
+   def new
+      @article=Article.new;
+   end
+
+   def create   
+      @article=Article.new(params.require(:article).permit(:title, :description));
+      #render plain: @article.inspect
+      if @article.save
+         flash[:alert]="Article was successfully created";
+         #redirect_to article_path(@article)
+         redirect_to Article
+      else
+         render :new
+      end      
+   end 
+   
+   def edit
+   end
+
+   def update
+   end
+      
 end    
